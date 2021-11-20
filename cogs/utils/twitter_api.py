135d11ae3ec2f436c5de2d9573afcd6cea8cf39a
XikoCat <guigasreis99@gmail.com>
2021-11-20 03:48:23 +0000

    Updated twitter api code

--- cogs/utils/twitter_api.py
+++ cogs/utils/twitter_api.py
@@ -1,5 +1,5 @@
 from discord.ext.commands.core import is_owner
-import twitter
+import tweepy
 import os
 from dotenv import load_dotenv
 
@@ -9,46 +9,27 @@ load_dotenv()
 # Keys and secrets can be get on https://developer.twitter.com/
 # an account needs to be made
 
-twitter = twitter.Api(
+auth = tweepy.OAuthHandler(
     consumer_key=os.getenv("Twitter_API_key"),
     consumer_secret=os.getenv("Twitter_API_secret"),
-    access_token_key=os.getenv("Twitter_Access_token_key"),
-    access_token_secret=os.getenv("Twitter_Access_token_secret"),
+)
+auth.set_access_token(
+    key=os.getenv("Twitter_Access_token_key"),
+    secret=os.getenv("Twitter_Access_token_secret"),
 )
 
-
-def user_by_id(id):
-    try:
-        user = twitter.GetUser(user_id=id)
-        return {"tag": user.id_str, "name": user.name}
-    except:
-        return None
-
-
-def user_by_screen_name(screen_name):
-    try:
-        user = twitter.GetUser(screen_name=screen_name)
-        return {"tag": user.id_str, "name": user.name}
-    except:
-        return None
+api = tweepy.API(auth)
 
 
 def get_user_info(account):
-    # if account is id
-    creator = user_by_id(account)
-    if creator is not None:
-        return creator
-
-    # if account is id
-    creator = user_by_screen_name(account)
-    if creator is not None:
-        return creator
-
-    return None
+    try:
+        return api.get_user(screen_name=account).id_str
+    except:
+        return None
 
 
 def get_latest_tweet(id):
-    user = twitter.GetUser(user_id=id)
+    user = api.get_user(user_id=id)
 
     username = user.name
     link = f"https://twitter.com/{user.screen_name}/status/{user.status.id}"
